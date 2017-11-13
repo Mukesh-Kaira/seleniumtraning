58a9ebbc2c95d7a0e62a0e338a2c41bc0b4c3ead
Mukesh-Kaira <33417598+Mukesh-Kaira@users.noreply.github.com>
2017-11-13 15:57:27 +0530

    Initial commit

---
+++ src/test/java/seleniumtraning/testing.java
@@ -0,0 +1,19 @@
+package seleniumtraning;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class testing {
+	@Test
+	public static void main()
+	{
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\mukesh.kaira\\workspace\\seleniumtraning\\src\\executable\\geckodriver.exe");
+		
+		WebDriver driver= new FirefoxDriver();
+		driver.get("https://www.google.co.in/");
+	}
+	
+	
+
+}
